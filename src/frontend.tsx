c542b1d98af41691298e1ade9882b34182cc4510
pomimon <bright.dusk1649@fastmail.com>
2026-01-05 14:40:10 -0800

    Remove css import

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -8,7 +8,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import "./index.css";
 
 const elem = document.getElementById("root")!;
 const app = (
