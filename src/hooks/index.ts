16514d79c4d2bd24e86e97a3f10f94c82256d7dd
pomimon <bright.dusk1649@fastmail.com>
2026-01-05 14:40:30 -0800

    Update hooks index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,4 @@
 export { usePokemonList } from "./usePokemonList";
 export { usePokemonDetail } from "./usePokemonDetail";
+export { usePokemonSpecies } from "./usePokemonSpecies";
+export { useEvolutionChain } from "./useEvolutionChain";
