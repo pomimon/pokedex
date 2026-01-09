12d95bce052ff3e672a0979ea198b680f347f157
pomimon <bright.dusk1649@fastmail.com>
2026-01-09 14:18:26 -0800

    Clean up unecessary code

--- src/components/CardSummary/index.tsx
+++ src/components/CardSummary/index.tsx
@@ -2,42 +2,23 @@ import styles from "./style.module.css";
 import type { PokemonInfo, PokemonType } from "@/types";
 import { TypeBadge } from "@/components/TypeBadge";
 import { capitalize, formatId, getSpriteUrl } from "@/utils";
+import { usePokemonStore } from "@/store";
 
-const PokeNumber = ({ id }: Pick<PokemonInfo, "id">) => {
-  return <div className={styles.number}>{formatId(id)}</div>;
-};
+type Props = Pick<PokemonInfo, "id" | "name" | "types">;
 
-const PokeSprite = ({ id, name }: Pick<PokemonInfo, "id" | "name">) => {
+export const CardSummary = ({ id, name, types }: Props) => {
+  const openModal = usePokemonStore((s) => s.openModal);
   const spriteUrl = getSpriteUrl(id);
-
-  return (
-    <div className={styles.sprite}>
-      <img src={spriteUrl} alt={name} loading="lazy" />
-    </div>
-  );
-};
-
-const PokeName = ({ name }: Pick<PokemonInfo, "name">) => {
-  return <h3 className={styles.name}>{capitalize(name)}</h3>;
-};
-
-const PokeType = ({ types }: Pick<PokemonInfo, "types">) => {
   const badges = types.map((type) => <TypeBadge key={type} type={type} />);
 
-  return <div className={styles.types}>{badges}</div>;
-};
-
-export const CardSummary = ({
-  id,
-  name,
-  types,
-}: Pick<PokemonInfo, "id" | "name" | "types">) => {
   return (
-    <div className={styles.card}>
-      <PokeNumber id={id} />
-      <PokeSprite id={id} name={name} />
-      <PokeName name={name} />
-      <PokeType types={types} />
+    <div className={styles.card} onClick={() => openModal(id)}>
+      <div className={styles.number}>{formatId(id)}</div>
+      <div className={styles.sprite}>
+        <img src={spriteUrl} alt={name} loading="lazy" />
+      </div>
+      <h3 className={styles.name}>{capitalize(name)}</h3>
+      <div className={styles.types}>{badges}</div>
     </div>
   );
 };
