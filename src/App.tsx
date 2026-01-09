92cc79593db274ca30b0e679f5632eedb5e8b333
pomimon <bright.dusk1649@fastmail.com>
2026-01-09 14:20:49 -0800

    Update app to show modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { useEffect } from "react";
 
 import { CardList } from "@/components/CardList";
+import { Modal } from "@/components/Modal";
+import { ModalDetails } from "@/components/ModalDetails";
 import { usePokemonStore } from "@/store";
 import type { PokemonInfo } from "@/types";
 
 export const App = () => {
   const fetch = usePokemonStore((store) => store.fetch);
+
+  const current = usePokemonStore((store) => store.current);
   const pokemon = usePokemonStore((store) => store.pokemon);
+
   const loadingAll = usePokemonStore((store) => store.loadingAll);
   const failureAll = usePokemonStore((store) => store.failureAll);
+
+  const modalOpen = usePokemonStore((store) => store.modalOpen);
+  const closeModal = usePokemonStore((store) => store.closeModal);
 
   useEffect(() => {
     fetch();
@@ -38,6 +46,10 @@ export const App = () => {
       <main>
         <CardList pokemon={pokemon} loading={loadingAll} />
       </main>
+
+      <Modal isOpen={modalOpen} onClose={closeModal}>
+        <ModalDetails pokemon={current} />
+      </Modal>
     </div>
   );
 };
