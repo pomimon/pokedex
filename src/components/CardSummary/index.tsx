c62f7de0e49dba5fc837a4712065ca9dd8ebb53d
pomimon <bright.dusk1649@fastmail.com>
2026-01-09 18:33:19 -0800

    Add sprite component

--- src/components/CardSummary/index.tsx
+++ src/components/CardSummary/index.tsx
@@ -3,6 +3,7 @@ import type { PokemonInfo, PokemonType } from "@/types";
 import { TypeBadge } from "@/components/TypeBadge";
 import { capitalize, formatId, getSpriteUrl } from "@/utils";
 import { usePokemonStore } from "@/store";
+import { SpriteImage } from "@/components/SpriteImage";
 
 type Props = Pick<PokemonInfo, "id" | "name" | "types">;
 
@@ -14,9 +15,7 @@ export const CardSummary = ({ id, name, types }: Props) => {
   return (
     <div className={styles.card} onClick={() => openModal(id)}>
       <div className={styles.number}>{formatId(id)}</div>
-      <div className={styles.sprite}>
-        <img src={spriteUrl} alt={name} loading="lazy" />
-      </div>
+      <SpriteImage id={id} name={name} size={120} />
       <h3 className={styles.name}>{capitalize(name)}</h3>
       <div className={styles.types}>{badges}</div>
     </div>
