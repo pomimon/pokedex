d071f4dd158e025a4f82816607b31c6c33bbccbd
pomimon <bright.dusk1649@fastmail.com>
2026-01-12 13:54:24 -0800

    Update badge with props for better reusability

--- src/components/TypeBadge/index.tsx
+++ src/components/TypeBadge/index.tsx
@@ -2,15 +2,19 @@ import styles from "./style.module.css";
 import { getTypeColor } from "@/utils";
 import { PokemonType } from "@/types";
 
+type TypeBadgeFormat = "button" | "pill";
+
 type TypeBadgeProps = {
   type: PokemonType;
+  format?: TypeBadgeFormat;
 };
 
-export const TypeBadge = ({ type }: TypeBadgeProps) => {
+export const TypeBadge = ({ type, format = "pill" }: TypeBadgeProps) => {
   const backgroundColor = getTypeColor(type);
+  const className = [styles.badge, styles[format]];
 
   return (
-    <span className={styles.badge} style={{ backgroundColor }}>
+    <span className={className.join(" ")} style={{ backgroundColor }}>
       {type}
     </span>
   );
