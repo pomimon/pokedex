f7e8c215cefdf32d606ea0ac1cdd86f5a1fc1463
pomimon <bright.dusk1649@fastmail.com>
2026-01-30 15:24:20 -0800

    Test getPokeColor utility

---
+++ src/tests/getPokeColor.test.js
@@ -0,0 +1,13 @@
+import { getPokeColor } from "../utils";
+
+test("get poke color from single types", () => {
+  const result = getPokeColor(["electric"]);
+  expect(result.typeColorA).toBe("#F8D030");
+  expect(result.typeColorB).toBe("#F8D030");
+});
+
+test("get poke color from two types", () => {
+  const result = getPokeColor(["electric", "steel"]);
+  expect(result.typeColorA).toBe("#F8D030");
+  expect(result.typeColorB).toBe("#B8B8D0");
+});
